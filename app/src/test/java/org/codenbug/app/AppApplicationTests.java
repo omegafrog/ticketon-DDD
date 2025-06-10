8d8d07124724d76e0439115c3bff5325ed0149d7
omegafrog <jiwoo22110103@gmail.com>
2025-06-10 17:47:58 +0900

    Init: first init

---
+++ app/src/test/java/org/codenbug/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codenbug.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
