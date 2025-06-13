8cdaf0d7ed30ae85a0ebac83b09c21f6e3a26c3c
omegafrog <jiwoo22110103@gmail.com>
2025-06-13 17:17:09 +0900

    feat: add event controller

--- event/src/main/java/org/codenbug/event/ui/EventController.java
+++ event/src/main/java/org/codenbug/event/ui/EventController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.codenbug.common.RsData;
 import org.codenbug.event.application.RegisterEventService;
+import org.codenbug.event.application.UpdateEventService;
 import org.codenbug.event.domain.EventId;
 import org.codenbug.event.global.NewEventRequest;
+import org.codenbug.event.global.UpdateEventRequest;
 import org.codenbug.seat.domain.SeatLayoutId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +27,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
 
 	private final RegisterEventService registerEventService;
+	private final UpdateEventService updateEventService;
 
-	public EventController(RegisterEventService registerEventService) {
+	public EventController(RegisterEventService registerEventService, UpdateEventService updateEventService) {
 		this.registerEventService = registerEventService;
+		this.updateEventService = updateEventService;
 	}
 	/**
 	 * 이벤트 등록 API
@@ -43,6 +48,30 @@ public class EventController {
 			"200",
 			"이벤트 등록 성공",
 			eventId
+		));
+	}
+	// @RoleRequired({UserRole.MANAGER, UserRole.ADMIN})
+	@PutMapping("/{eventId}")
+	public ResponseEntity<RsData<EventId>> updateEvent(
+		@PathVariable String eventId,
+		@RequestBody UpdateEventRequest request
+	) {
+		updateEventService.updateEvent(new EventId(eventId), request);
+		return ResponseEntity.ok(new RsData<>(
+			"200",
+			"이벤트 수정 성공",
+			new EventId(eventId)
+		));
+	}
+
+	// @RoleRequired({UserRole.MANAGER, UserRole.ADMIN})
+	@PatchMapping("/{eventId}")
+	public ResponseEntity<RsData<Void>> deleteEvent(@PathVariable String eventId) throws IllegalAccessException {
+		updateEventService.deleteEvent(new EventId(eventId));
+		return ResponseEntity.ok(new RsData<>(
+			"200",
+			"이벤트 삭제 성공",
+			null
 		));
 	}
 }
