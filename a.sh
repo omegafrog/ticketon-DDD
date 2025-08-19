8547fe4b9239148a981df3c04923243093a64009
Kim Ji Woo <jiwoo22110103@gmail.com>
2025-08-19 22:46:26 +0900

    delete: delete unused files

--- a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for (( i=0; i<1050; i=i+1 ))
-  do
-    curl -X POST http://localhost:8080/api/v1/auth/login \
-          -H "Content-Type: application/json" \
-          -d '{"email": "user1@ticketon.site", "password":
-        "password123"}'
-done
