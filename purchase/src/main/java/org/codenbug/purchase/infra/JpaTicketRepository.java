a5d401c196a936ea8fec3c6c91b954e1afff5b34
omegafrog <jiwoo22110103@gmail.com>
2026-03-26 17:41:30 +0900

    refactor(purchase): align ticket repository key and nplus1 fixtures

--- purchase/src/main/java/org/codenbug/purchase/infra/JpaTicketRepository.java
+++ purchase/src/main/java/org/codenbug/purchase/infra/JpaTicketRepository.java
@@ -1,7 +1,8 @@
 package org.codenbug.purchase.infra;
 
 import org.codenbug.purchase.domain.Ticket;
+import org.codenbug.purchase.domain.TicketId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaTicketRepository extends JpaRepository<Ticket, String> {
+public interface JpaTicketRepository extends JpaRepository<Ticket, TicketId> {
 }
